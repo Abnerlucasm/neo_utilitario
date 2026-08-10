0debc03e098307ad4f2f2d98e7ace15275e26992
ilsonwar <ilsonwarsc@gmail.com>
2026-08-10 11:56:31 -0300

    feat: Adicionado card para acesso a nova tela de gerenciamento de APIs bancarias

--- models/postgresql/Server.js
+++ models/postgresql/Server.js
@@ -3,9 +3,9 @@ const { sequelize } = require('../../config/database');
 
 const Server = sequelize.define('Server', {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
     name: {
         type: DataTypes.STRING(100),
